7182891acea03bc8a2faadb1251374a60dfd4101
Katarina Chiam <katarina.chiam@mail.utoronto.ca>
2022-11-30 20:56:35 +0000

    added edge cases for if prediction is over the highest value/under the lowest value in the atlas, if prediction matches on the hhghest/lowest value and case handling when prediction matches an image in atlas results in vertically stacked grid (3x1) of lower end and higher end

--- app/atlas.py
+++ app/atlas.py
@@ -15,21 +15,46 @@ class Atlas:
                             for sex, path in self._atlas_paths.items()}
         self._font = ImageFont.truetype("Arial.ttf", 100)
 
+    # def case_am
+
     def render(self, original_xray: np.ndarray, sex: str,
                age: int) -> np.ndarray:
-        less_val, more_val = self._get_closest_nums(sex, age)
+
+        even_lower, less_val, more_val = self._get_closest_nums(sex, age)
+        # if prediction (age) = atlas age, send one lower and one higher (i.e: [lower, image, match] and [match, image, higher])
+        # for the sake of simplicity, we will stack them on top of each other (6 imgs in 1 series)
+        # print(f'{even_lower}, {less_val}, {age}, {more_val}')
+        # if it matches, by default, the match will be in less_val so we have taken care of the [match, image, higher] case
 
         image = Image.fromarray(original_xray, 'L')
         draw = ImageDraw.Draw(image)
         age_years, age_months = divmod(age, 12)
-        draw.text((50, 80), f'{age_years}y {age_months}m', fill="#fff",
+        format_pred = f'{age_years}y {age_months}m' if age_months != 0 else f'{age_years}y'
+        draw.text((50, 80), format_pred, fill="#fff",
                   font=self._font, stroke_width=5, stroke_fill="#000")
 
+        if even_lower != None: # happens if we have a match
+            lowest_img = Image.open(os.path.join(self._atlas_paths[sex], f'{even_lower}.png'))  
+            lowest_img = lowest_img.resize((image.size[1], image.size[1]))
+        else:
+            lowest_img = None
+        
         less_img = Image.open(os.path.join(self._atlas_paths[sex], f'{less_val}.png'))
         less_img = less_img.resize((image.size[1], image.size[1]))
         more_img = Image.open(os.path.join(self._atlas_paths[sex], f'{more_val}.png'))
         more_img = more_img.resize((image.size[1], image.size[1]))
 
+        if lowest_img == None:
+            return self._make_grid(less_img, image, more_img)
+        else:
+            lower = Image.fromarray(self._make_grid(lowest_img, image, less_img), 'L')
+            higher = Image.fromarray(self._make_grid(less_img, image, more_img), 'L')
+            combined_img = Image.new('L', (lower.width, lower.height + higher.height))
+            combined_img.paste(lower, (0, 0))
+            combined_img.paste(higher, (0, lower.height))
+            return np.array(combined_img)
+    
+    def _make_grid(self, less_img, image, more_img):
         # https://stackoverflow.com/questions/30227466/combine-several-images-horizontally-with-python
         images = [less_img, image, more_img]
         widths, heights = zip(*(i.size for i in images))
@@ -49,15 +74,19 @@ class Atlas:
         # https://stackoverflow.com/questions/2566412/find-nearest-value-in-numpy-array
         closest_idx = (np.abs(self._atlas_nums[sex] - age)).argmin()
         closest_val = self._atlas_nums[sex][closest_idx]
-        if closest_val > age:
+        even_lower = None
+        if (closest_val > age and closest_val != self._atlas_nums[sex][0]) or closest_val == self._atlas_nums[sex][-1]: # if closest value is the largest val in atlas
             more_idx = closest_idx
             less_idx = closest_idx - 1
         else:
+            if closest_val == age and closest_val != self._atlas_nums[sex][0]: # we can only go lower if the closest val is not the min value in atlas
+                lower_idx = closest_idx - 1
+                even_lower = self._atlas_nums[sex][lower_idx]
             more_idx = closest_idx + 1
             less_idx = closest_idx
         less_val = self._atlas_nums[sex][less_idx]
         more_val = self._atlas_nums[sex][more_idx]
-        return less_val, more_val
+        return even_lower, less_val, more_val
 
 
 def _get_atlas_nums(path: str) -> np.ndarray:
